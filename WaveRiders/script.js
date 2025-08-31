9298b17b0410dc8e9f3fb67a4cdce2052b854800
Annamani <annamanik.vit@gmail.com>
2025-08-31 16:55:46 +0200

    script added

---
+++ WaveRiders/script.js
@@ -0,0 +1,9 @@
+const book_lesson=document.getElementsByClassName('lesson_btn')[0];
+book_lesson.addEventListener("click",()=>{
+       alert('You have booked the lesson');
+})
+
+const contact_button=document.getElementsByClassName('contact_btn')[0];
+contact_button.addEventListener("click",()=>{
+    alert('Thanks for contacting us. We will get back to you soon!');
+})
